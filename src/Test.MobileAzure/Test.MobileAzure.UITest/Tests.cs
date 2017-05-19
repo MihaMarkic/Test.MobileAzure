a2f6daba4003db33d5ff6a39575aa3f00e6d9d3e
Miha Markič <miha@rthand.com>
2017-05-19 16:34:16 +0200

    Adds UI Test

---
+++ src/Test.MobileAzure/Test.MobileAzure.UITest/Tests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Android;
+
+namespace Test.MobileAzure.UITest
+{
+    [TestFixture]
+    public class Tests
+    {
+        AndroidApp app;
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            // TODO: If the Android app being tested is included in the solution then open
+            // the Unit Tests window, right click Test Apps, select Add App Project
+            // and select the app projects that should be tested.
+            app = ConfigureApp
+                .Android
+                .Debug()
+                //.ApkFile("../../../Test.MobileAzure/bin/Debug/Test.MobileAzure.Test.MobileAzure.apk")
+                //.ApkFile()
+                .InstalledApp("Test.MobileAzure.Test.MobileAzure")
+                .StartApp();
+        }
+
+        [Test]
+        public void AppLaunches()
+        {
+            app.WaitForElement(c => c.Marked("MyButton"));
+            app.Tap(c => c.Marked("MyButton"));
+            //app.Repl();
+            app.WaitForElement(c => c.Marked("MyButton").Text("1 clicks!"));
+            //app.Screenshot("First screen.");
+        }
+    }
+}
+
