181595306f0c3fc8b569336847afdda57ed381db
Miha Markič <miha@rthand.com>
2017-05-19 15:30:26 +0200

    Initial

---
+++ src/Test.MobileAzure/Test.MobileAzure.UnitTests/Class1.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Test.MobileAzure.UnitTests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void AssertTrue()
+        {
+            Assert.True(true);
+        }
+    }
+}
